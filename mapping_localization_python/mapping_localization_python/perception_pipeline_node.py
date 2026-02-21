90fdb8877051ae4419c35178c71b1db188385d67
Maharshii Patel <maharshiip13@gmail.com>
2026-02-21 13:22:42 -0500

    test key

--- mapping_localization_python/mapping_localization_python/perception_pipeline_node.py
+++ mapping_localization_python/mapping_localization_python/perception_pipeline_node.py
@@ -46,6 +46,8 @@ class PerceptionPipelineNode(Node):
         self._min_lane_points = int(
             self.declare_parameter('min_lane_points', 2).value
         )
+
+        # Queue length is one, we don't need a queue per say
         self._qos_depth = int(self.declare_parameter('qos_depth', 1).value)
         self._cached_left_boundary: list[Point] = []
         self._cached_right_boundary: list[Point] = []
